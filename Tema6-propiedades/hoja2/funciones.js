baae80c2a7956d0fe46986a66b143e05394b3f2a
Liferbis <liferbis@gmail.com>
2015-11-20 11:05:36 +0100

    hojas propiedades String completas

--- Tema6-propiedades/hoja2/funciones.js
+++ Tema6-propiedades/hoja2/funciones.js
@@ -1,7 +1,8 @@
 var c = "";
+var f;
 var frase;
 var pala;
-
+var palabra = "";
 var posicion = 0;
 
 
@@ -13,11 +14,20 @@ function carga(){
 
 function fuera(){
 	document.getElementById("caden").setAttribute('hidden','hidden');
+	//document.getElementById("fas").setAttribute('hidden','hidden');
 	document.getElementById("bot").setAttribute('hidden','hidden');
 	}
 
 function myCad(){
 	c = document.getElementById("cad").value;
+}
+
+function myFra(){
+	f = Number(document.getElementById("fra").value);
+}
+
+function myPa(){
+	palabra = document.getElementById("pa").value;
 }
 
 function crea(){
@@ -75,6 +85,38 @@ function maxL(){
 }
 
 function efrase(){
+	var texto="Elige una frase: </br><ol>";
+	
+	for(var i = 0; i < frase.length; i++){
+		texto +="<li>" + frase[i] + " </li>";			
+	}
 
+	texto += "</ol></br>Introduce el número de la frase:</br><input type=number id='fra' min='1' max='" + frase.length + "' oninput='myFra()' placeholder='Número de la frase'><button class='botones' type='button' id='fras' onclick='fras()'>Deletrear frase!!</button>";
+	document.getElementById('respuesta').innerHTML = texto;
+}
+
+function fras(){
+	f = f-1;
+	var palabras = frase[f].split(" ");
+	var texto="Elige una palabra: </br>";
+	
+	for(var i = 0; i < palabras.length; i++){
+		texto +="<li>" + palabras[i] + " </li>";			
+	}
+
+	texto += "</br>Introduce la palabra que quieres deletrear:</br><input type='text' id='pa'  oninput='myPa()' placeholder='Introduce la palabra'><button class='botones' type='button' id='del' onclick='del()'>Deletrear palabra!!</button>";
+	document.getElementById('respuesta').innerHTML = texto;
 
 }
+
+function del(){
+	var texto = "<strong>" + palabra + "</strong><table class='table'><thead><tr><th>Posición</th><th>Letra</th></tr></thead><tbody>";
+	for (var i = 0; i < palabra.length; i++) {
+		texto += "<tr><td>" + i + "</td><td>" + palabra.charAt(i) + "</td></tr>"; 
+	}
+	texto += "</tbody></table>";
+	document.getElementById('respuesta').innerHTML = texto;
+}
+			
+		
+	
