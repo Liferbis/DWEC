97533b8e060a4d3fa808711951324dc02caa0a63
Liferbis <liferbis@gmail.com>
2015-11-23 20:15:15 +0100

    puto array

--- LidiaFernandez_Examen2/Ejercicio1/include/factura.js
+++ LidiaFernandez_Examen2/Ejercicio1/include/factura.js
@@ -4,36 +4,49 @@ function factura(empresa, cliente, infor){
 	this.infor = infor;
 	this.linea= new Array();
 
-	this.AgregarElementos=AgregarElementos;
-	this.CalculaFactura=CalculaFactura;
-	this.ver=ver;
+	this.AgregarElementos = AgregarElementos;
+	this.CalculaFactura = CalculaFactura;
+	this.ver = ver;
 	this.muestra = muestra;
+	this.line = line;
 }
 
 function AgregarElementos(descripcion, precio, cantidad){
-	var aux=new Array(descripcion, precio, cantidad);
+	var aux = new Array(descripcion, precio, cantidad);
 	this.linea.push(aux);
 }
 
 function CalculaFactura(){
 	var aux=0;
 	var precioT=0;
-	var iv=this.infor.IVA;
+	var iv=(this.infor.IVA/100)+1;
 	for(variable in this.linea){
-		aux=(Number(this.linea[variable][1])*iv)*0.1;
+		aux = Number(this.linea[variable][1]*iv);
 		aux = aux*(this.linea[variable][2]);
-		precioT = Number(aux); 
+		precioT += Number(aux); 
 	}
 	this.infor.importeT=precioT;
 }
 
+function line(){
+	var texto="<table class='prod'><thead><tr><th>Articulo</th><th>Precio</th><th>Cantidad</th></tr></thead><tbody>";
+
+	for(variable in this.linea){
+		texto +="<tr><td>" + this.linea[variable][0] + "</td>";
+		texto +="<td>" + this.linea[variable][1] + "</td>";
+		texto +="<td>" + this.linea[variable][2] + "</td></tr>"; 
+	}
+	texto += "</tbody></table>";
+
+	return texto;
+}
+
 function ver(){
 	var n =Number(this.infor.importeT);
-	return n;
-	
+	return n;	
 }
 
 function muestra() { 
-	var cadena = "Datos Empresa: <br><strong>" + this.empresa.mue() + "</strong><br>Datos Cliente: <br><strong>" + this.cliente.mir() + " </strong><br> Información: <strong>"+ this.infor.tot() + "</strong>";
+	var cadena = "<article class='empresa'>Datos Empresa: <br>" + this.empresa.mue() + "</article><article class='cliente'>Datos Cliente: <br>" + this.cliente.mir() + "</article><article >" + this.line() + "</article><article class='infor'> Información: "+ this.infor.tot() + "</article><article class='im'><img src='include/captura.png' alt='imagen' />";
 	return(cadena); 
 }
--- LidiaFernandez_Examen2/Ejercicio1/include/funciones.js
+++ LidiaFernandez_Examen2/Ejercicio1/include/funciones.js
@@ -1,7 +1,8 @@
 var d="";
 var p = 0;
 var c = 0;
-var fac;
+var n = 0;
+var fac = new Array();
 
 addEventListener('load', carga, false);
 
@@ -14,8 +15,17 @@ function carga(){
 
 	var inf = new infor(0, 21, "Tarjeta de credito");
 
-	fac = new factura(em, cl, inf);
+	var em1 = new empresa("tu", "C/ La Peste", "600-606-666", "75-C");
+	
+	var cl1 = new cliente("Popi", "C/ La Rabioa", "666-000-696", "13-D");
 
+	var inf1 = new infor(0, 21, "Al contado");
+
+	var fa = new factura(em, cl, inf);
+	var fa1 = new factura(em1, cl1, inf1);
+
+	fac.push(fa);
+	fac.push(fa1);
 }
 
 function fuera(){
@@ -38,34 +48,60 @@ function ag(){
 	document.getElementById("datos").removeAttribute('hidden');
 }
 
+function myFact(){
+	n = Number(document.getElementById("fra").value);
+	n = n-1;
+}
+
 function agrega(){
 	fuera();
+	var texto="Elige una factura: </br><ol>";
+	
+	for(var i = 0; i < fac.length; i++){
+		texto +="<li>" + fac[i].empresa.nombreE + " </li>";			
+	}
+
+	texto += "</ol></br>Introduce el número de la factura:</br><input type=number id='fra' min='1' max='" + fac.length + "' oninput='myFact()' placeholder='Número de la factura'><button class='botones' type='button' id='Agr' onclick='agre()'>Introduce datos!!</button>";
+	document.getElementById('respuesta').innerHTML = texto;
+}
+
+function agre(){
 	var texto = "";
+	
 	if(d == "" || p == "" || c ==""){
 		texto="<h3 class='resM'>HAY DATOS INCORRECTOS O VACIOS!! <br> No se guarda ningun dato!!</h3>";
 	}else{
-		fac.AgregarElementos(d,p,c);
+		fac[n].AgregarElementos(d,p,c);
 		texto="<h3 class='resB'>Agregado correctamente!!</h3>";	
 	}
 	document.getElementById('respuesta').innerHTML = texto;
 }
 
 function calcula(){
-	fac.CalculaFactura();
+	for(var i = 0; i < fac.length; i++){
+		fac[i].CalculaFactura();
+	}
 	var texto="<h3 class='resB'>Calculado correctamente!!</h3>";
 	document.getElementById('respuesta').innerHTML = texto;
 }
 
 function mostrar(){
-	var texto="<p class='respuesta'>";
-		texto += fac.muestra();
-		texto += "</p>";
+	var texto = " ";
+	for(var i = 0; i < fac.length; i++){
+	 	texto += "<section class='respuesta'>";
+	 	texto += fac[i].muestra();
+	 	texto += "</section>";
+	 }
 	document.getElementById('respuesta').innerHTML = texto;
+	// document.getElementById("respuesta").className = "respuesta";
 }
 
 function vr(){
-	var texto="<h3 class='resB'>Precio Total:  ";
-	 	texto += fac.ver();
+	var texto =" ";
+	for(var i = 0; i < fac.length; i++){
+		texto="<h3 class='resB'>Precio Total factura(" + (i+1) +"):  ";
+	 	texto += fac[i].ver();
 	 	texto += "  !!</h3>";
+	}
 	document.getElementById('respuesta').innerHTML = texto;
 }
